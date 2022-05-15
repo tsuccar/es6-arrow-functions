c4cd6a78bbc63043a09b61e13e0f98a82b278571
tsuccar <tsuccar@users.noreply.github.com>
2022-05-15 07:47:18 -0400

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      description={emojiTerm.meaning}
-    />
-  );
-}
+// Old function before transformation
+
+// function createEntry(emojiTerm) {
+//   return (
+//     <Entry
+//       key={emojiTerm.id}
+//       emoji={emojiTerm.emoji}
+//       name={emojiTerm.name}
+//       description={emojiTerm.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -19,7 +21,16 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,29 +4,53 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
 // const newNumbers = numbers.map(function (x) {
 //   return x * 2;
 // });
 
-//////Filter - Create a new array by keeping the items that return true.
+// Solution
+// const newNumbers = numbers.map( x => x * 2 );
+
+// console.log(newNumbers);
+
+////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
 //   return num < 10;
 // });
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+//Solution
+// const newNumbers = numbers.filter( num => num < 10 );
+
+// console.log(newNumbers);
+
+// Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
 //     return accumulator + currentNumber;
 // })
+
+//Solution
+// var newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber)
+
+// console.log (newNumber)
 
 ////Find - find the first item that matches from an array.
 // const newNumber = numbers.find(function (num) {
 //   return num > 10;
 // })
 
+//Solution
+// const newNumber = numbers.find( num => num > 10)
+
+// console.log(newNumber)
+
 ////FindIndex - find the index of the first item that matches.
 // const newNumber = numbers.findIndex(function (num) {
 //   return num > 10;
 // })
+
+//Solution
+// const newNumber = numbers.findIndex( num => num > 10)
+// console.log(newNumber)
